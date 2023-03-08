ae16f777096b654c0bb2734b8b6a43c6d5e7968d
Davi Feliciano <dfeliciano37@gmail.com>
2023-03-08 19:32:04 -0300

    Adding Contact Loader

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Root, {
   loader as rootLoader,
   action as rootAction,
 } from "./routes/root";
-import Contact from "./routes/contact";
+import Contact, { loader as contactLoader } from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
       {
         path: "/contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },
