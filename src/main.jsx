a38b3ff018c68fcf569a4b1a03c98759265f04b1
Davi Feliciano <dfeliciano37@gmail.com>
2023-03-08 18:03:30 -0300

    Formatting files

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import "./index.css";
-import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
 import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
@@ -19,9 +19,9 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/contacts/:contactId",
-        element: <Contact />
-      }
-    ]
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
